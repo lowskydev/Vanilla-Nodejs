06c129f13123843a4ccb1fe33dbffc6e7ebe3efa
lowskydev <lowskydev@gmail.com>
2026-03-06 23:53:35 +0100

    implement individual user API

--- server2.js
+++ server2.js
@@ -11,13 +11,26 @@ const users = [
 const server = createServer((req, res) => {
   if (req.url === "/api/users" && req.method === "GET") {
     res.setHeader("Content-Type", "application/json");
-    console.log(JSON.stringify(users));
     res.write(JSON.stringify(users));
     res.end();
+  } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
+    const id = req.url.split("/")[3];
+    const user = users.find((user) => user.id === parseInt(id));
+
+    if (user) {
+      res.setHeader("Content-Type", "application/json");
+      res.write(JSON.stringify(user));
+      res.end();
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.statusCode = 404;
+      res.write(JSON.stringify({ message: "User not found" }));
+      res.end();
+    }
   } else {
     res.setHeader("Content-Type", "application/json");
     res.statusCode = 404;
-    res.write(JSON.stringify({ message: "not found" }));
+    res.write(JSON.stringify({ message: "page not found" }));
     res.end();
   }
 });
