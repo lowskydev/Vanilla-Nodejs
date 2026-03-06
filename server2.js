d5f579c263444584cc9855b85d5a2e5c06804c7e
lowskydev <lowskydev@gmail.com>
2026-03-06 23:39:41 +0100

    create users API

---
+++ server2.js
@@ -0,0 +1,26 @@
+import { createServer } from "http";
+
+const PORT = process.env.PORT;
+
+const users = [
+  { id: 1, name: "John Doe" },
+  { id: 2, name: "Jane Doe" },
+  { id: 3, name: "Jim Doe" },
+];
+
+const server = createServer((req, res) => {
+  if (req.url === "/api/users" && req.method === "GET") {
+    res.setHeader("Content-Type", "application/json");
+    console.log(JSON.stringify(users));
+    res.write(JSON.stringify(users));
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "application/json" });
+    res.write(JSON.stringify({ message: "not found" }));
+    res.end();
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
