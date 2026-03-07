d0d98bbc38e0cf4f94a7bd2e119b8b5f72d02ef8
lowskydev <lowskydev@gmail.com>
2026-03-07 01:22:03 +0100

    explore os module methods

--- osDemo.js
+++ osDemo.js
@@ -1,1 +1,14 @@
 import os from "os";
+
+// userInfo()
+console.log(os.userInfo());
+console.log(os.userInfo().username);
+
+// totalmem()
+console.log(`${os.totalmem() / 1024 / 1024 / 1024} GB`); // in Bytes to GB
+
+// freemem()
+console.log(os.freemem());
+
+// cpus()
+console.log(os.cpus());
